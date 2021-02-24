8b02385a57abb1ce2abc5c6222908da8d695b05e
santiago-rodrig <santo1996.29@gmail.com>
2021-02-23 22:46:32 -0400

    completed the blog

---
+++ components/profileImage.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function ProfileImage() {
+  <Image
+    src="/images/profile.jpg"
+    height={144}
+    width={144}
+    alt="Profile Image"
+  />
+}
+
